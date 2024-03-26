fff0dff08bf12e753bc2406a14d593f83b60b0a0
Leonard Greulich <leonard.greulich@gmail.com>
2024-03-26 15:57:23 +0100

    feat: add initial database schema

--- db/config.ts
+++ db/config.ts
@@ -1,6 +1,25 @@
-import { defineDb } from "astro:db";
+import { defineDb, defineTable, column } from "astro:db";
+
+const Event = defineTable({
+    columns: {
+        id: column.text({ primaryKey: true }),
+        icon: column.text(),
+        name: column.text(),
+        description: column.text(),
+        date: column.date()
+    }
+});
+
+const Participant = defineTable({
+    columns: {
+        eventId: column.text({ references: () => Event.columns.id }),
+        name: column.text(),
+        isAdmin: column.boolean(),
+        willParticipate: column.boolean()
+    }
+});
 
 // https://astro.build/db/config
 export default defineDb({
-    tables: {}
+    tables: { Event, Participant }
 });
