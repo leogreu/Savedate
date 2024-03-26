48c73d4ca64b217ad6d2d5a40f088a6511f70dbe
Leonard Greulich <leonard.greulich@gmail.com>
2024-03-26 14:44:21 +0100

    feat: add event form

--- src/components/ui/date-picker.tsx
+++ src/components/ui/date-picker.tsx
@@ -20,12 +20,12 @@ export function DatePicker() {
                 <Button
                     variant={"outline"}
                     className={cn(
-                        "w-[280px] justify-start text-left font-normal",
+                        "justify-start text-left font-normal",
                         !date && "text-muted-foreground"
                     )}
                 >
                     <CalendarIcon className="mr-2 h-4 w-4" />
-                    {date ? format(date, "PPP") : <span>Pick a date</span>}
+                    {date ? format(date, "PPP") : "Datum auswählen"}
                 </Button>
             </PopoverTrigger>
             <PopoverContent className="w-auto p-0">
