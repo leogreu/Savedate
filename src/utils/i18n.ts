d3198b3107b68407720b80bb985f0016ed3f9375
Leonard Greulich <leonard.greulich@gmail.com>
2024-04-01 12:30:22 +0200

    fix: preferred locale detection

--- src/utils/i18n.ts
+++ src/utils/i18n.ts
@@ -4,6 +4,14 @@ const translations = Object.fromEntries(
     (await getCollection("translations")).map(entry => [entry.id, entry.data])
 );
 
+// Required due to a bug in Astro.preferredLocale, not identifying regional codes (e.g., de-DE)
+export const getPreferredLocale = (headers: Headers) => {
+    return headers.get("accept-language")
+        ?.split(",")
+        .map(lang => lang.split(";")[0].split("-")[0])
+        .at(0);
+};
+
 export const getTranslations = (locale = "en") => {
     return (key: string) => {
         return translations[locale][key] ?? translations["en"][key] ?? key;
