ede2aa2e785ad79b792732bc306f8031b7a20a67
Leonard Greulich <leonard.greulich@gmail.com>
2024-04-04 19:05:37 +0200

    feat: only set cookie on post

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,7 @@ import { defineMiddleware } from "astro:middleware";
 import { nanoid } from "nanoid";
 
 export const onRequest = defineMiddleware((context, next) => {
-    if (!context.cookies.get("savedate")) {
+    if (context.request.method === "POST" && !context.cookies.get("savedate")) {
         // Set cookie with one year age to be able to redirect to previous responses
         context.cookies.set("savedate", nanoid(24), { maxAge: 60 * 60 * 24 * 365 });
     }
