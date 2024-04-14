d2f870d18d7b0c8dcb3c16695fdf7f6437d15c2c
Leonard Greulich <leonard.greulich@gmail.com>
2024-04-14 18:23:41 +0200

    fix: explicitly include custom font

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,9 @@ import vercel from "@astrojs/vercel/serverless";
 // https://astro.build/config
 export default defineConfig({
     output: "server",
-    adapter: vercel(),
+    adapter: vercel({
+        includeFiles: ["./assets/NotoSans-Bold.ttf"]
+    }),
     i18n: {
         defaultLocale: "en",
         locales: ["en", "de"]
