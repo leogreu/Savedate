39b3ac64ee2102adc46e0c42cb8abd58f948d928
Leonard Greulich <leonard.greulich@gmail.com>
2024-04-14 17:41:16 +0200

    build: deploy to netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,21 +2,17 @@ import { defineConfig } from "astro/config";
 import db from "@astrojs/db";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
     output: "server",
-    adapter: vercel(),
+    adapter: netlify(),
     i18n: {
         defaultLocale: "en",
         locales: ["en", "de"]
     },
-    integrations: [
-        db(),
-        react(),
-        tailwind({
-            applyBaseStyles: false
-        })
-    ]
+    integrations: [db(), react(), tailwind({
+        applyBaseStyles: false
+    })]
 });
